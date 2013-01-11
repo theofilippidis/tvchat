ed3955c48af1e3024601cd40c34f11275e3f870f
theofilippidis <theo@filippidis.biz>
2013-01-11 18:03:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<html>
+<head>
+<title>TVchat</title>
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=441160562621946";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <?php
 echo "Hello World :)";
 ?>
+
+<div class="fb-comments" data-href="http://example.com" data-width="470" data-num-posts="25"></div>
+</body>
+
