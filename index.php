0ea1e48733aee9e5c201522f6b733b46afbaa72a
theofilippidis <theo@filippidis.biz>
2013-01-12 07:18:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,10 @@
 <title>TVchat</title>
 </head>
 <body>
- 
- <FRAME src="www.google.com">
- 
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=441160562621946";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 <?php
 echo "Hello World :)";
 ?>
 
-<div class="fb-comments" data-href="http://example.com" data-width="470" data-num-posts="25"></div>
 </body>
 
