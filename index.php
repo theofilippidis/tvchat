3a1e8d83b07fce034f16c14db7752cb794c22caf
Theo Filippidis <theo@filippidis.biz>
2013-01-14 23:18:13 +0200

    Removed Heroku address

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
    $app_id = "441160562621946";
   // $app_secret = "79b785ec4ed3530fb0cd9965fa9320e2";
-   $my_url = "https://tvchat.herokuapp.com/chat.php";
+   $my_url = "https://TVchat.gr/chat.php";
 
    session_start();
 
