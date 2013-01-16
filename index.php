36dbde5b59615f8f1da229dd14fc0cb90cefe953
Theo Filippidis <theo@filippidis.biz>
2013-01-16 06:26:52 +0200

    scope=null

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
      $_SESSION['state'] = md5(uniqid(rand(), TRUE)); // CSRF protection
      $dialog_url = "https://www.facebook.com/dialog/oauth?client_id="
        . $app_id . "&redirect_uri=" . urlencode($my_url) . "&state="
-       . $_SESSION['state']. "&scope=email,user_likes,friends_likes";
+       . $_SESSION['state']; //. "&scope=email,user_likes,friends_likes";
 
      echo("<script> top.location.href='" . $dialog_url . "'</script>");
    }
